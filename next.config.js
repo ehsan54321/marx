bd5724009a28050dbc77dcb3ec5c1048b2b454a9
ehsan54321 <ehsan.2009.12@gmail.com>
2024-01-17 04:33:22 -0800

    This is a new commit for what I originally planned to be amended

--- next.config.js
+++ next.config.js
@@ -2,23 +2,23 @@
 const { i18n } = require('./next-i18next.config')
 const prod = process.env.NODE_ENV === 'production'
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  disable: prod ? false : true,
-  skipWaiting: true,
+    dest: 'public',
+    register: true,
+    disable: prod ? false : true,
+    skipWaiting: true,
 })
 
 const settings = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n,
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
-  experimental: {
-    // for liara services in production
-    outputStandalone: true,
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n,
+    // typescript: {
+    //   ignoreBuildErrors: true,
+    // },
+    experimental: {
+        // for liara services in production
+        outputStandalone: true,
+    },
 }
 
 module.exports = withPWA(settings)
